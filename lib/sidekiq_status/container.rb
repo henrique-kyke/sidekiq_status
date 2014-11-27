57245160a68fc0b3bd6ec2492db865ffd9232666
Henrique Santana <henrique.santana@outlook.com>
2014-11-27 10:58:14 -0200

    Ajuste ttl

--- lib/sidekiq_status/container.rb
+++ lib/sidekiq_status/container.rb
@@ -23,7 +23,7 @@ module SidekiqStatus
     STATUSES_KEY          = 'sidekiq_statuses'.freeze
 
     class_attribute :ttl
-    self.ttl = 0 # 30 days
+    self.ttl = 30 # 30 days
 
     # Default attribute values (assigned to a newly created container if not explicitly defined)
     DEFAULTS = {
